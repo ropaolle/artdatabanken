d3917f2556ffb4df529a1bc5932e04dcb3cf4233
ropaolle <ropaolle@gmail.com>
2023-06-19 17:01:50 +0200

    upload images

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { useState } from 'react';
-import './App.css';
+// import './App.css';
 import Navigation from './components/Navigation.tsx';
 import Footer from './components/Footer.tsx';
 import UploadImage from './components/UploadImage.tsx';
+import ImageList from './components/ImageList.tsx';
 
 function App() {
   const [openUploadDialog, setOpenUploadDialog] = useState(false);
+  // const [openImageDialog, setOpenImageDialog] = useState(false);
 
   return (
     <>
-      <Navigation show={() => setOpenUploadDialog(true)} />
+      <Navigation show={() => setOpenUploadDialog(true)} images={() => setOpenImageDialog(true)} />
       <main className="container">
-        <h1>Artdatabanken 2023</h1>
-        <UploadImage open={openUploadDialog} show={setOpenUploadDialog}/>
+        <h1>Sidgenerator</h1>
+        {/* <h2>Bilder</h2> */}
+        <UploadImage open={openUploadDialog} show={setOpenUploadDialog} />
+        {/* <ImageList open={openImageDialog} show={setOpenImageDialog} /> */}
       </main>
       <Footer />
     </>
