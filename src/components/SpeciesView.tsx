0a62c3bcfbb8ee0e5b31addde296d00df3cf50c4
ropaolle <ropaolle@gmail.com>
2023-06-27 19:18:50 +0200

    hk

--- src/components/SpeciesView.tsx
+++ src/components/SpeciesView.tsx
@@ -1,47 +1,44 @@
 import { useState, useEffect } from 'react';
 import { type SpeciesInfo } from '../lib/firebase';
+import { Icon } from '@iconify/react';
+
+type Filters = Omit<SpeciesInfo, 'updatedAt'>;
 
 type Props = {
   species: SpeciesInfo[];
 };
 
-type SpeciesFilters = Omit<SpeciesInfo, 'updatedAt'>;
-
-const defaultFilters = {
-  kingdom: '',
-  order: '',
-  family: '',
-  species: 'a',
-  sex: '',
-  county: '',
-  place: '',
-  speciesLatin: '',
-  image: '',
-  date: '',
-};
-
 export default function SpeciesView({ species }: Props) {
-  // const [sort, setSort] = useState<string>();
-  const [filters, setFilter] = useState(defaultFilters);
-  const [items, setItems] = useState<SpeciesInfo[]>();
+  const [sort, setSort] = useState({ column: '', ascending: true });
+  const [filters, setFilter] = useState({ kingdom: '' });
+  const [items, setItems] = useState(species);
 
   useEffect(() => {
-    const applyFilters = (data: SpeciesFilters[]) =>
-      data.filter((item) =>
-        Object.entries(filters).every(([key, value]) => {
-          if (typeof key !== 'string' || key === '') return true;
-          const property = item[key as keyof SpeciesFilters];
-          return property.includes(value);
-        })
-      );
+    const filteredSpecies = species.filter((item) =>
+      Object.entries(filters).every(([key, value]) => {
+        if (typeof key !== 'string' || key === '') return true;
+        // FIXA: updatedAt omitted as this only works on filters with string values
+        const property = item[key as keyof Filters];
+        return property.toLowerCase().includes(value.toLowerCase());
+      })
+    );
 
-    setItems(applyFilters(species));
-    // console.log('filter');
+    setItems(filteredSpecies);
   }, [species, filters]);
 
-  const speciesTable =
-    items &&
-    items.map(({ kingdom, order, family, species, sex, speciesLatin, place, county, date, image, updatedAt }) => (
+  const localeSort = (a: SpeciesInfo, b: SpeciesInfo) => {
+    // Ignore sort
+    if (sort.column === '') return 0;
+    // Compare columns
+    const column = sort.column as keyof Filters;
+    const localeCompare = a[column].localeCompare(b[column], 'sv', { sensitivity: 'base' });
+    // Sort order
+    return sort.ascending ? -localeCompare : localeCompare;
+  };
+
+  const speciesTable = items
+    .sort(localeSort)
+    .map(({ kingdom, order, family, species, sex, speciesLatin, place, county, date, image /* , updatedAt */ }) => (
       <tr key={species}>
         <td>{kingdom}</td>
         <td>{order}</td>
@@ -49,15 +46,19 @@ export default function SpeciesView({ species }: Props) {
         <td>{species}</td>
         <td>{sex}</td>
         <td>{speciesLatin}</td>
+        {/* <td>
+          <div>{place}</div>
+          <div>{county}</div>
+          <div>{date}</div>
+        </td> */}
         <td>{place}</td>
         <td>{county}</td>
         <td>{date}</td>
-        {/* <td>{updatedAt.toDate().toLocaleDateString()}</td> */}
         <td>{image}</td>
       </tr>
     ));
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement> | undefined) => {
+  const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement> | undefined) => {
     setFilter((prevValues) => {
       const id = e?.target.id;
       const value = e?.target.value || '';
@@ -66,17 +67,39 @@ export default function SpeciesView({ species }: Props) {
     });
   };
 
+  const handleTableSortClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, id: string) => {
+    e.preventDefault();
+    setSort({ column: id, ascending: sort.column === id ? !sort.ascending : sort.ascending });
+  };
+
+  const HeaderCell = ({ header, id }: { header: string; id: string }) => (
+    <th>
+      <a href="#" onClick={(e) => handleTableSortClick(e, id)}>
+        {header}
+        {sort.column === id && (
+          <Icon icon={`material-symbols:keyboard-arrow-${sort.ascending ? 'up' : 'down'}-rounded`} />
+        )}
+      </a>
+    </th>
+  );
+
   return (
     <div className="species-view">
       <form>
         <div className="grid">
+          {/* <label htmlFor="all">
+            Filter
+            <input id="all" onChange={handleFilterChange} />
+          </label> */}
+
           <label htmlFor="species">
             Art
-            <input id="species" onChange={onChange} />
+            <input id="species" onChange={handleFilterChange} />
           </label>
+
           <label htmlFor="speciesLatin">
             Latinskt namn
-            <input id="speciesLatin" onChange={onChange} />
+            <input id="speciesLatin" onChange={handleFilterChange} />
           </label>
 
           <div></div>
@@ -88,17 +111,16 @@ export default function SpeciesView({ species }: Props) {
         <table className="species-table" role="grid">
           <thead>
             <tr>
-              <th>Klass</th>
-              <th>Ordning</th>
-              <th>Familj</th>
-              <th>Art</th>
-              <th>Kön</th>
-              <th>Latinskt namn</th>
-              <th>Lokal</th>
-              <th>Län</th>
-              <th>Datum</th>
-              {/* <th>Uppdaterad</th> */}
-              <th>Bild</th>
+              <HeaderCell header="Klass" id="kingdom" />
+              <HeaderCell header="Order" id="order" />
+              <HeaderCell header="Familj" id="family" />
+              <HeaderCell header="Art" id="species" />
+              <HeaderCell header="Kön" id="sex" />
+              <HeaderCell header="Latinskt namn" id="speciesLatin" />
+              <HeaderCell header="Lokal" id="place" />
+              <HeaderCell header="Län" id="county" />
+              <HeaderCell header="Datum" id="date" />
+              <HeaderCell header="Bild" id="image" />
             </tr>
           </thead>
           <tbody>{speciesTable}</tbody>
