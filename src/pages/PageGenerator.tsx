6fd3f46503620ac1d948ec22347a87589ee7a968
Olof Sjögren <ropaolle@gmail.com>
2023-07-05 21:52:49 +0200

    hk

--- src/pages/PageGenerator.tsx
+++ src/pages/PageGenerator.tsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import Page from './Page';
-import { importData } from '../lib/firebase';
+import { importData, importImages } from '../lib/firebase';
 
 export default function PageGenerator() {
+  const [images, setImages] = useState<FileList>();
+
+  const onHandleFileChange = (e: React.ChangeEvent<HTMLInputElement> /*  | undefined */) => {
+    // console.log('e', e.currentTarget.files);
+    setImages(e.currentTarget.files || undefined);
+  };
+
+  const handleImport = () => {
+    if (images && images[0]) {
+      importImages(images, 'images2022');
+      console.log('images', images);
+    }
+  };
+
   return (
-    <Page title="Sidgenerator" headerButtonTitle="Ladd upp..." onHeaderButtonClick={importData}>
+    <Page title="Sidgenerator" headerButtonTitle="Ladd upp..." onHeaderButtonClick={handleImport}>
       Innehåll ...
+      <input type="file" onChange={onHandleFileChange} multiple />
     </Page>
   );
 }
