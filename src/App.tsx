218f95c7298014d4ff263aa79e94303d938b4daf
Olof Sjögren <ropaolle@gmail.com>
2023-07-26 19:30:39 +0200

    ver 0.0.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css';
-import { useState, useEffect /* , lazy, Suspense */ } from 'react';
+import { useState, useEffect, lazy, Suspense } from 'react';
 import { Timestamp } from 'firebase/firestore/lite';
-import { Home, ImageView, SpeciesView, Collections, Settings, type PAGES } from './pages';
+import { /* Home, ImageView, SpeciesView, Collections, Settings, */ type PAGES } from './pages';
 // import { type PAGES } from './pages';
 import { Navigation, Footer, Auth } from './components';
 // import { firestoreFetchDoc, COLLECTIONS } from './lib/firebase';
 import { useAppStore, fetchGlobalState } from './state';
 
 function App() {
-  const { initGlobalState/* , globalStateFetchedAt */ } = useAppStore();
-  const [page, setPage] = useState<PAGES>('SPECIES');
+  const { initGlobalState /* , globalStateFetchedAt */ } = useAppStore();
+  const [page, setPage] = useState<PAGES>('HOME');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,7 +21,7 @@ function App() {
       // const fullUpdate = !globalStateFetchedAt || databaseUpdatedAt > globalStateFetchedAt;
       const { images, species } = await fetchGlobalState(/* fullUpdate */);
       console.log('images.length', images.length);
-      
+
       initGlobalState(images, species, Timestamp.now());
     };
 
@@ -29,23 +29,23 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // const Home = lazy(() => import('./pages/Home'));
-  // const SpeciesView = lazy(() => import('./pages/SpeciesView/SpeciesView'));
-  // const ImageView = lazy(() => import('./pages/ImageView'));
-  // const Collections = lazy(() => import('./pages/Collections/Collections'));
-  // const Settings = lazy(() => import('./pages/Settings'));
+  const Home = lazy(() => import('./pages/Home'));
+  const SpeciesView = lazy(() => import('./pages/SpeciesView/SpeciesView'));
+  const ImageView = lazy(() => import('./pages/ImageView'));
+  const Collections = lazy(() => import('./pages/Collections/Collections'));
+  const Settings = lazy(() => import('./pages/Settings'));
 
   return (
     <>
       <Auth />
       <Navigation setPage={setPage} />
-      {/* <Suspense fallback={<div>Olle</div>}> */}
-      {page === 'HOME' && <Home setPage={setPage} />}
-      {page === 'SPECIES' && <SpeciesView />}
-      {page === 'IMAGES' && <ImageView />}
-      {page === 'COLLECTIONS' && <Collections />}
-      {page === 'SETTINGS' && <Settings />}
-      {/* </Suspense> */}
+      <Suspense fallback={<div>Olle</div>}>
+        {page === 'HOME' && <Home setPage={setPage} />}
+        {page === 'SPECIES' && <SpeciesView />}
+        {page === 'IMAGES' && <ImageView />}
+        {page === 'COLLECTIONS' && <Collections />}
+        {page === 'SETTINGS' && <Settings />}
+      </Suspense>
       <Footer />
     </>
   );
